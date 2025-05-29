c600e09c2935c443f40cb8a616d6d669902141d3
RunalB <rbanarse@concoursesols.com>
2025-05-30 01:22:07 +0530

    banner carousel

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  currentSlide = 0;
+  slides = [
+    {
+      title: 'Delicious Food Delivered',
+      description: 'Order your favorite meals online and get them delivered to your doorstep',
+      background: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'Fresh & Healthy Meals',
+      description: 'Enjoy our wide selection of fresh and healthy food options',
+      background: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'Special Offers',
+      description: 'Get amazing deals and discounts on your favorite dishes',
+      background: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    }
+  ];
 
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }, 3000);
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
 }
